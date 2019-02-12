c5146c76a04335af6190e054a0f4194a9cfde5c0
nyirahabineza <habcecile@gmail.com>
2019-02-12 15:54:47 +0200

    adding class TestUser user_test.py

--- user_test.py
+++ user_test.py
@@ -1,7 +1,6 @@
 import unittest # Importing the unittest module
 from user import User # Importing the user class
 
-class TestUser(unittest.TestCase):
 
     '''
     Test class that defines test cases for the user class behaviours.
